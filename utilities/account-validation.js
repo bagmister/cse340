e757b10b4be4925edd8ec8b30e0b3201e3013129
kevin bagley <bagmister@yahoo.com>
2026-02-12 22:04:22 -0700

    updates

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -4,32 +4,28 @@ const utilities = require(".")
 
   validate.registationRules = () => {
     return [
-      // firstname is required and must be string
       body("account_firstname")
         .trim()
         .escape()
         .notEmpty()
         .isLength({ min: 1 })
-        .withMessage("Please provide a first name."), // on error this message is sent.
+        .withMessage("Please provide a first name."),
   
-      // lastname is required and must be string
       body("account_lastname")
         .trim()
         .escape()
         .notEmpty()
         .isLength({ min: 2 })
-        .withMessage("Please provide a last name."), // on error this message is sent.
+        .withMessage("Please provide a last name."),
   
-      // valid email is required and cannot already exist in the DB
       body("account_email")
       .trim()
       .escape()
       .notEmpty()
       .isEmail()
-      .normalizeEmail() // refer to validator.js docs
+      .normalizeEmail()
       .withMessage("A valid email is required."),
   
-      // password is required and must be strong password
       body("account_password")
         .trim()
         .notEmpty()
